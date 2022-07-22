489dddc6a022280dc8bd06277cab47437089d3d5
Sarah Hall <swatson1994@yahoo.com>
2022-07-22 11:55:43 -0400

    Update facade test to include desired length

--- spec/facades/avatar_facade_spec.rb
+++ spec/facades/avatar_facade_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe AvatarFacade do
     characters = AvatarFacade.create_affiliation_characters("Fire+Nation")
 
     expect(characters[0]).to be_an_instance_of(Character)
+    expect(characters.length).to eq(25)
   end
 end
