2ac6f5f7f6b8a1fccb6741ee1ce00ec04ad4ca38
Sarah Hall <swatson1994@yahoo.com>
2022-07-22 12:37:41 -0400

    Test: Add test for details, and amount on page

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -7,6 +7,19 @@ RSpec.describe 'Search Result Page', type: :feature do
     click_button('Search For Members')
   end
   it 'shows total number of fire benders' do
-    expect(page).to have_content('Total Fire Nation Member Count: 100~')
+    expect(page).to have_content('Total Fire Nation Member Count: 97')
+  end
+
+  it 'lists twenty five characters affiliated with search choice' do
+    expect(page.all('.character').length).to eq(25)
+  end
+
+  it 'lists character details' do
+    within page.all('.character')[0] do
+      expect(page).to have_content("Ozai")
+      expect(page).to have_content("Earth Kingdom")
+      expect(page).to have_content("Chan")
+      expect(page).to have_content("Fire Nation Navy")
+    end
   end
 end
