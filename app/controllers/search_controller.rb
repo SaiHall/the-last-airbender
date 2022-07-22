338c20e59d26924ab1e7c9bc08007e1507bc29c1
Sarah Hall <swatson1994@yahoo.com>
2022-07-22 12:38:00 -0400

    Update view and controller to include characters and details

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+    @characters = AvatarFacade.create_affiliation_characters(nation_params)
+  end
+
+  private
+
+  def nation_params
+    params[:nation].gsub(/_/, '+')
   end
 end
