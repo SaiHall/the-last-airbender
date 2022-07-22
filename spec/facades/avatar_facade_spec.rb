4e00c2029eceedcf0cc1fbecb6ef64e5d88cf5d0
Sarah Hall <swatson1994@yahoo.com>
2022-07-22 11:53:08 -0400

    Test: Add initial avatar facade test

---
+++ spec/facades/avatar_facade_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe AvatarFacade do
+  it 'creates a character poro from affiliation call' do
+    characters = AvatarFacade.create_affiliation_characters("Fire+Nation")
+
+    expect(characters[0]).to be_an_instance_of(Character)
+  end
+end
