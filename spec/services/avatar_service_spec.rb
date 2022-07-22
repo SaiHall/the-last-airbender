5184cf3ce572fa66ddec56d2cc7494cc7bed9dc1
Sarah Hall <swatson1994@yahoo.com>
2022-07-22 11:45:20 -0400

    Test: Add initial service test for get Affilitation

---
+++ spec/services/avatar_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe AvatarService do
+  describe 'Affiliation Endpoint' do
+    it 'can retrieve data from nation affiliation endpoint' do
+      json = AvatarService.get_affilitaion("Fire+Nation")
+
+      expect(json).to be_a(Array)
+      expect(json[0]).to have_key(:name)
+    end
+  end
+end
